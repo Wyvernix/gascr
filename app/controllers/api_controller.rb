f9cc042c7b9abdfd47cda1e6cd3c16b51c1eab21
Brandon Gastelo <bagastelo@cpp.edu>
2019-12-02 12:47:25 -0800

    Cleanup

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -8,85 +8,10 @@ class ApiController < ApplicationController
   def brent
     @brent_price = Datum.find(tag: :oil)
 
-
     if stale?(last_modified: @brent_price.updated_at)
       render json: @brent_price
     end
     # @ibrent_price = get_brent
-  end
-
-  def populate_data
-    puts "Reloading EIA dataset..."
-    petrol = JSON.load(open("http://api.eia.gov/series/?api_key=#{ENV['EIA_KEY']}&series_id=PET.EER_EPMRU_PF4_Y35NY_DPG.W"))
-    oil = JSON.load(open("http://api.eia.gov/series/?api_key=#{ENV['EIA_KEY']}&series_id=PET.RWTC.W"))
-    la = JSON.load(open("http://api.eia.gov/series/?api_key=#{ENV['EIA_KEY']}&series_id=PET.EMM_EPM0_PTE_Y05LA_DPG.W"))
-
-    petrol.dig('series', 0, 'data').each do |datum|
-      # 0: date, 1: value
-      Statistic.create_with(value: datum[1].to_f).find_or_create_by(
-        period: datum[0].to_i,
-        series: 'petrol'
-      )
-#      stat = Statistic.find_or_create_by(
-#        period: datum[0].to_i,
-#        series: 'petrol')
-#      stat.value = datum[1].to_f
-#      stat.save
-    end
-    puts "Loaded petrol data."
-
-    oil.dig('series', 0, 'data').each do |datum|
-      Statistic.create_with(value: datum[1].to_f).find_or_create_by(
-        period: datum[0].to_i,
-        series: 'oil'
-      )
-    end
-
-    la.dig('series', 0, 'data').each do |datum|
-      Statistic.create_with(value: datum[1].to_f).find_or_create_by(
-        period: datum[0].to_i,
-        series: 'la'
-      )
-    end
-
-    Statistic.find_or_create_by(series: 'log')
-      .update_attribute(:value, Time.current.to_i)
-    puts "Loaded oil data"
-  end
-
-  def load_data
-    last = Statistic.find_by(series: 'log')
-    populate_data unless (last && last.value > 1.week.ago.to_i)
-
-    @oil = Statistic.order(period: :desc).where(series: 'oil').limit(3)
-    @petrol = Statistic.order(period: :desc).where(series: 'la').limit(3)
-  end
-
-  def find_trend
-    load_data
-    x = Math.log(@oil[0].value)
-    y = Math.log(@oil[1].value)
-    z = Math.log(@oil[2].value)
-    a = Math.log(@petrol[0].value)
-    b = Math.log(@petrol[1].value)
-    c = Math.log(@petrol[2].value)
-
-    dpetrol = 0.198 * (a-b) + -0.086 * (b-c)
-    dxy = x-y
-    dyz = y-z
-    dxy *= (dxy.positive?) ? 0.739 : 1.325
-    dyz *= (dyz.positive?) ? -0.256 : -0.095
-
-    dpetrol + dxy + dyz
-  end
-
-  def day_trend
-    case Date.current.day
-    when 4..6
-      0.01
-    else
-      -0.006
-    end
   end
 
   def trends
@@ -105,6 +30,75 @@ class ApiController < ApplicationController
   end
 
   private
+
+  def open_json(series_id)
+    JSON.parse(
+      open("http://api.eia.gov/series/?api_key=#{ENV['EIA_KEY']}&series_id=#{series_id}").read
+    )
+  end
+
+  def push_json(json, series)
+    data = json.dig('series', 0, 'data')
+    return if data.nil?
+
+    data.each do |datum|
+      Statistic.create_with(value: datum[1].to_f).find_or_create_by(
+        period: datum[0].to_i,
+        series: series
+      )
+    end
+    puts "Loaded #{series} data"
+  end
+
+  def populate_data
+    puts 'Reloading EIA dataset...'
+    petrol = open_json('PET.EER_EPMRU_PF4_Y35NY_DPG.W')
+    oil = open_json('PET.RWTC.W')
+    la = open_json('PET.EMM_EPM0_PTE_Y05LA_DPG.W')
+
+    push_json(petrol, 'petrol')
+    push_json(oil, 'oil')
+    push_json(la, 'la')
+
+    Statistic.find_or_create_by(series: 'log')
+             .update_attribute(:value, Time.current.to_i)
+    puts 'Loaded oil data'
+  end
+
+  def load_data
+    last = Statistic.find_by(series: 'log')
+    populate_data unless last && last.value > 1.week.ago.to_i
+
+    @oil = Statistic.order(period: :desc).where(series: 'oil').limit(3)
+    @petrol = Statistic.order(period: :desc).where(series: 'la').limit(3)
+  end
+
+  def find_trend
+    load_data
+    x = Math.log(@oil[0].value)
+    y = Math.log(@oil[1].value)
+    z = Math.log(@oil[2].value)
+    a = Math.log(@petrol[0].value)
+    b = Math.log(@petrol[1].value)
+    c = Math.log(@petrol[2].value)
+
+    dpetrol = 0.198 * (a - b) + -0.086 * (b - c)
+    dxy = x - y
+    dxy *= dxy.positive? ? 0.739 : 1.325
+    dyz = y - z
+    dyz *= dyz.positive? ? -0.256 : -0.095
+
+    dpetrol + dxy + dyz
+  end
+
+  def day_trend
+    case Date.current.day
+    when 4..6
+      0.01
+    else
+      -0.006
+    end
+  end
 
   def oil
     doc = Nokogiri(open('https://markets.businessinsider.com/commodities/oil-price'))
